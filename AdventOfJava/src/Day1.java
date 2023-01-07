62b6aff4d22fedc717f44fbd7b7e88d261b4ce3f
William Goble <will.goble@gmail.com>
2023-01-06 21:04:10 -0500

    Adds documentation to functions

--- AdventOfJava/src/Day1.java
+++ AdventOfJava/src/Day1.java
@@ -5,7 +5,25 @@ import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
+/**
+ * @author William Goble
+ */
+
 public class Day1 {
+    /**
+     * The jungle must be too overgrown and difficult to navigate in vehicles 
+     * or access from the air; the Elves' expedition traditionally goes on foot.
+     * As your boats approach land, the Elves begin taking inventory of their 
+     * supplies. One important consideration is food - in particular, the 
+     * number of Calories each Elf is carrying (your puzzle input).
+     * 
+     * The Elves take turns writing down the number of Calories contained by 
+     * the various meals, snacks, rations, etc. that they've brought with them, 
+     * one item per line. Each Elf separates their own inventory from the 
+     * previous Elf's inventory (if any) by a blank line.
+     * @param args
+     *  args will contain the filename of our inputs for the day
+     */
     public static void main(String[] args) {
         if (args.length != 1) {
 			System.out.println("usage: java Day1 path/to/file.txt");
@@ -23,6 +41,14 @@ public class Day1 {
         System.out.println("Solution 2:\t" + solution2);
     }
 
+    /**
+     * Helper function to create a priority queue in descending order
+     * 
+     * @param filename
+     *      String: string literal of our input source file.
+     * @return
+     *      Priority Queue in descending order 
+     */
     public static PriorityQueue<Integer> sortElves(String filename) {
         PriorityQueue<Integer> answer = new PriorityQueue<Integer>(Collections.reverseOrder());
 
@@ -47,11 +73,31 @@ public class Day1 {
         return answer;
     }
 
+    /**
+     * In case the Elves get hungry and need extra snacks, they need to know 
+     * which Elf to ask: they'd like to know how many Calories are being carried
+     * by the Elf carrying the most Calories. 
+     * 
+     * Find the Elf carrying the most Calories. How many total Calories is that 
+     * Elf carrying?
+     * @param elves
+     *      Sorted priority queue of all the elves calories
+     * @return
+     *      the highest calorie count
+     */
     public static int part1(PriorityQueue<Integer> elves) {
         int answer = elves.peek();
         return answer;
     }
 
+    /**
+     * Find the top three Elves carrying the most Calories.How many Calories are 
+     * those Elves carrying in total?
+     * @param calories
+     *      Sorted priority queue of all the elves calories
+     * @return
+     *      the summation of the three highest calorie counts. 
+     */
     public static int part2(PriorityQueue<Integer> calories) {
         int val1 = calories.poll();
         int val2 = calories.poll();
