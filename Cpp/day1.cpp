d69aac694428ee14d571e8c9ffc0918381d86a6f
William Goble <will.goble@gmail.com>
2023-01-09 13:53:23 -0500

    adds ability to pass in string literal as function argument

--- Cpp/day1.cpp
+++ Cpp/day1.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+void printPhrase(const std::string& word) {
+    std::cout << word << std::endl;
+}
+
 int main(){
     std::cout << "Hello World!\n";
+    printPhrase("This is a test");
 
     return 0;
 }
